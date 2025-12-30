8d1241b1fe6ecb587e9d58bdb9a2e2e4b6f8198f
jos3lo89 <ganbaru743@gmail.com>
2025-12-30 15:03:17 -0500

    refactor:  user module

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug'],
+  });
 
   app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
